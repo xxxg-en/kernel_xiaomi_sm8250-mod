60d952ecfd18b9fd51d3adfa4af565fa729004a5
Zhang Qilong <zhangqilong3@huawei.com>
2022-09-14 09:33:22 +0800

    f2fs: code clean and fix a type error
    ERROR: code indent should use tabs where possible
    ERROR: spaces required around that ':'
    ERROR: incorrect tab
    
    Found serveral code type errors when review the code and fix it.
    There is no function change.
    
    Signed-off-by: Zhang Qilong <zhangqilong3@huawei.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -734,7 +734,7 @@ int f2fs_submit_page_bio(struct f2fs_io_info *fio)
 	bio_set_op_attrs(bio, fio->op, fio->op_flags);
 
 	inc_page_count(fio->sbi, is_read_io(fio->op) ?
-			__read_io_type(page): WB_DATA_TYPE(fio->page));
+			__read_io_type(page) : WB_DATA_TYPE(fio->page));
 
 	__submit_bio(fio->sbi, bio, fio->type);
 	return 0;
