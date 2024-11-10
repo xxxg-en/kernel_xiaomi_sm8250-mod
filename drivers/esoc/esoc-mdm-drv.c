46e6cd594a59b6808aef341cf60f978bf962b43f
Yanfeng Lee <liyafe1997@126.com>
2024-11-10 20:07:23 +0000

    Use esoc driver from pipa-t-oss

--- drivers/esoc/esoc-mdm-drv.c
+++ drivers/esoc/esoc-mdm-drv.c
@@ -17,11 +17,7 @@
 
 #define ESOC_MAX_PON_TRIES	5
 
-#ifdef CONFIG_MACH_XIAOMI_SM8250
 #define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_NOP
-#else
-#define BOOT_FAIL_ACTION_DEF BOOT_FAIL_ACTION_PANIC
-#endif
 
 enum esoc_pon_state {
 	PON_INIT,
@@ -75,16 +71,14 @@ int esoc_set_boot_fail_action(struct esoc_clink *esoc_clink, u32 action)
 		return -EINVAL;
 	}
 
-	if (action == BOOT_FAIL_ACTION_PANIC)
-		action = BOOT_FAIL_ACTION_DEF;
-
 	if (!mdm_drv) {
 		esoc_mdm_log("esoc-mdm driver not present\n");
 		return -EAGAIN;
 	}
 
-	atomic_set(&mdm_drv->boot_fail_action, action);
+	atomic_set(&mdm_drv->boot_fail_action, BOOT_FAIL_ACTION_NOP);
 	esoc_mdm_log("Boot fail action configured to %u\n", action);
+	dev_err(&esoc_clink->dev, "Boot fail action configured to %u\n", action);
 
 	return 0;
 }
@@ -396,7 +390,7 @@ static int mdm_handle_boot_fail(struct esoc_clink *esoc_clink, u8 *pon_trial)
 	if (*pon_trial == atomic_read(&mdm_drv->n_pon_tries)) {
 		esoc_mdm_log("Reached max. number of boot trials\n");
 		atomic_set(&mdm_drv->boot_fail_action,
-					BOOT_FAIL_ACTION_DEF);
+					BOOT_FAIL_ACTION_NOP);
 	}
 
 	switch (atomic_read(&mdm_drv->boot_fail_action)) {
